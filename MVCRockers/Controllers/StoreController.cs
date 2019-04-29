2a5f0080e4960bf87fb1a16fa460229f84c0809e
Saiful Islam <saifulislamlk@outlook.com>
2019-04-30 04:30:47 +0600

    Add Store

---
+++ MVCRockers/Controllers/StoreController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCRockers.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public string Index(string genre)
+        {
+            string message = HttpUtility.HtmlEncode("Store.browse(), genre = " + genre);
+            return message;
+        }
+
+        public string Browse()
+        {
+            return "Hello from store.Browse()";
+        }
+
+        public string Details()
+        {
+            return "Hello from store.Details()";
+        }
+    }
+}
